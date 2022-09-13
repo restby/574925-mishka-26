9c2b8b73814aa9128d236938035f84295bde3a5c
restby <restby@gmail.com>
2022-09-14 01:11:11 +0300

    исправления для допуска

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,16 +1,26 @@
-let navigationMenu = document.querySelector('.navigation__container');
+let navigation = document.querySelector('.navigation');
+let navigationMenu = navigation.querySelector('.navigation__wrapper');
 let navigationButton = document.querySelector('.navigation__toggle');
 let navigationBtnIconOpen = navigationButton.querySelector('.navigation__toggle-icon--open');
 let navigationBtnIconClose = navigationButton.querySelector('.navigation__toggle-icon--close');
 let mapWrapper = document.querySelector('.map__wrapper--no-js')
+let contactsImg = document.querySelector('.contacts__img');
 
-navigationMenu.classList.remove('navigation__container--no-js');
+
+navigation.classList.remove('navigation--no-js');
+navigationMenu.classList.remove('navigation__wrapper--no-js');
 navigationButton.classList.remove('navigation__toggle--no-js');
-mapWrapper.classList.remove('map__wrapper--no-js');
+
+if (mapWrapper) {
+  mapWrapper.classList.remove('map__wrapper--no-js');
+}
+if (contactsImg) {
+  contactsImg.classList.remove('contacts__img--no-js');
+}
 
 navigationButton.addEventListener('click', function (evt) {
   evt.preventDefault();
-  navigationMenu.classList.toggle('navigation__container--open');
+  navigationMenu.classList.toggle('navigation__wrapper--open');
   navigationBtnIconOpen.classList.toggle('navigation__toggle-icon--hide');
   navigationBtnIconClose.classList.toggle('navigation__toggle-icon--hide');
 });
