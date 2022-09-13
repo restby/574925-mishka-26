5ef5dc44bab44f1939fb2a0c4ba80d90b8e9b37d
restby <restby@gmail.com>
2022-09-13 12:23:56 +0300

    исправляет доступность карты

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -2,9 +2,11 @@ let navigationMenu = document.querySelector('.navigation__container');
 let navigationButton = document.querySelector('.navigation__toggle');
 let navigationBtnIconOpen = navigationButton.querySelector('.navigation__toggle-icon--open');
 let navigationBtnIconClose = navigationButton.querySelector('.navigation__toggle-icon--close');
+let mapWrapper = document.querySelector('.map__wrapper--no-js')
 
 navigationMenu.classList.remove('navigation__container--no-js');
 navigationButton.classList.remove('navigation__toggle--no-js');
+mapWrapper.classList.remove('map__wrapper--no-js');
 
 navigationButton.addEventListener('click', function (evt) {
   evt.preventDefault();
